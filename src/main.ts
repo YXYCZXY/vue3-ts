27b410df464980920438d8ce38bf0e5ca34da982
mangtanghua <245174862@qq.com>
2024-02-04 15:35:37 +0800

    配置 updata

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import piniaStore from './store';
+import router from './router';
 import 'uno.css';
+import Idux from '@/config/idux';
 // vue3 挂载
 const app = createApp(App)
 app.use(piniaStore);
-
+app.use(router);
+app.use(Idux);
 app.mount('#app')
